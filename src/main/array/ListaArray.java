2d57445daa50522221901427210cca1a012d6fa5
gustavof0411 <136523482+gustavof0411@users.noreply.github.com>
2023-11-10 01:22:25 +0000

    Testes sucedidos, faltando algumas implementações

--- src/main/array/ListaArray.java
+++ src/main/array/ListaArray.java
@@ -4,22 +4,19 @@ import list.EstruturaElementar;
 
 public class ListaArray implements EstruturaElementar{
 
-    private int[] array;
+    private int[] array = new int[1];
     public int indiceAposFim = 1;
 
     public ListaArray() {
-        this.array = new int[100];
     }
 
     @Override
     public boolean buscaElemento(int valor) { // Completo, testado.
-        if (indiceAposFim != 1) {
             for (int i = 0; i < indiceAposFim; i++) {
                 if (array[i] == valor) {
                     return true;
                 }
             }
-        }
         return false;
     }
 
@@ -28,11 +25,11 @@ public class ListaArray implements EstruturaElementar{
         if (valor < array.length) { // Verifica se o índice é menor ou igual ao tamanho do array.
             return array[valor];
         }
-        return -1; // Verificar retorno (pode enganar).
+        return -0; // Retorna -1 se o elemento não estiver no array.
     }
 
     @Override
-    public int minimo() { // Completo, testado.
+    public int minimo() { // Completo, testado, corrigido.
         int minimo = array[0];
         if (indiceAposFim != 1) { // Verifica se ao menos um elemento já foi adicionado.
             for (int i = 0; i < indiceAposFim; i++) {
@@ -45,7 +42,7 @@ public class ListaArray implements EstruturaElementar{
     }
 
     @Override
-    public int maximo() { // Completo, testado.
+    public int maximo() { // Completo, testado, corrigido.
         int maximo = array[0];
         if (indiceAposFim > 1) { // Verifica se ao menos um elemento já foi adicionado.
             for (int i = 0; i < indiceAposFim; i++) {
@@ -75,51 +72,62 @@ public class ListaArray implements EstruturaElementar{
     }
 
     @Override
-    public void insereElemento(int valor) { // Completo, testado.
-        if (indiceAposFim < array.length) {
-            for (int i = indiceAposFim - 1; i >= 0; i--) { // Está inserindo no começo.
-                array[i + 1] = array[i];
+    public void insereElemento(int valor) { // Completo, testado, corrigido.
+        int[] arrayCopia = new int[this.array.length+1];
+            for (int i = 0; i < indiceAposFim - 1; i++) { // Está inserindo no começo.
+                arrayCopia[i+1] = this.array[i];
             }
-            array[0] = valor;
+            arrayCopia[0] = valor;
+            this.array = arrayCopia;
             indiceAposFim += 1;
         }
-    }
 
     @Override
-    public void insereElementoPosicao(int valor, int buscaIndice) { // Completo.
-        if ((buscaIndice < indiceAposFim) && (indiceAposFim < array.length)) {
-            for (int i = indiceAposFim - 1; i <= buscaIndice; i--) {
-                array[i + 1] = array[i];
+    public void insereElementoPosicao(int valor, int buscaIndice) { // Completo, testado, corrigido.
+            int[] arrayCopia = new int[this.array.length+1];
+            for (int i = 0; i < buscaIndice; i++) {
+                arrayCopia[i] = array[i];
             }
-            array[buscaIndice] = valor;
-            indiceAposFim = buscaIndice + 1;
-        } else if (buscaIndice >= indiceAposFim && buscaIndice < array.length) {
-            array[buscaIndice] = valor;
-            indiceAposFim = buscaIndice + 1;
-
+            arrayCopia[buscaIndice] = valor;
+            for (int i = buscaIndice + 1; i <= indiceAposFim; i++) {
+                arrayCopia[i] = array[i-1];
+            }
+            indiceAposFim++;
+            this.array = arrayCopia;
         }
-    }
+
 
     @Override
-    public void insereInicio(int valor) { // Completo, testado.
+    public void insereInicio(int valor) { // Completo, testado, corrigido.
+        if (indiceAposFim != 1) {
+        int[] arrayCopia = new int[this.array.length+1];
         for (int i = indiceAposFim - 1; i >= 0; i--) {
-            array[i + 1] = array[i];
+            arrayCopia[i + 1] = this.array[i];
         }
-        array[0] = valor;
+        arrayCopia[0] = valor;
         indiceAposFim += 1;
+        this.array = arrayCopia;
+    } else {
+        this.array[0] = valor;
     }
+}
 
     @Override
-    public void insereFim(int valor) { // Completo.
-        if (indiceAposFim < array.length) {
-            array[indiceAposFim] = valor;
-            indiceAposFim += 1;
+    public void insereFim(int valor) { // Completo, testado, corrigido.
+        int[] arrayCopia = new int[this.array.length+1];
+        for (int i = 0; i < array.length; i++) {
+            arrayCopia[i] = this.array[i];
         }
+            arrayCopia[indiceAposFim - 1] = valor;
+            this.array = arrayCopia;
+            indiceAposFim += 1;
+
     }
 
     @Override
     public void removeIndice(int indice) { // Completo, testado.
         if (indice <= (indiceAposFim - 1)) {
+            int[] arrayCopia = new int[this.array.length+1];
             for (int i = indice; i < indiceAposFim - 1; i++) {
                 array[i] = array[i + 1];
             }
@@ -128,26 +136,48 @@ public class ListaArray implements EstruturaElementar{
         }
     }
 
-    public void remove(int valor) {
-
+    public void remove(int valor) { //Completo, testado, corrigido.
+        if (buscaElemento(valor)) { // Verifica se o valor existe no array.
+            int[] arrayCopia = new int[this.array.length-1];
+            int indiceElemRemovido = 0;
+            for (int i = 0; i < indiceAposFim; i++){
+                if (array[i] == valor) {
+                    indiceElemRemovido = i; //Salva o índice para continuar o processo.
+                    break;
+                } else {
+                    arrayCopia[i] = this.array[i];
+                }
+            }
+            for (int i = indiceElemRemovido+1; i < indiceAposFim; i++) {
+                arrayCopia[i-1] = this.array[i];
+            }
+            this.array = arrayCopia;
+            indiceAposFim--;
     }
+}
 
     @Override
-    public void removeInicio() { // Completo, testado.
+    public void removeInicio() { // Completo, testado, corrigido.
         if (indiceAposFim != 1) {
-            for (int i = 0; i < indiceAposFim; i++) {
-                array[i] = array[i + 1];
+            int[] arrayCopia = new int[this.array.length-1];
+            for (int i = 1; i < indiceAposFim; i++) {
+                arrayCopia[i-1] = this.array[i];
             }
-            array[indiceAposFim + 1] = 0;
+            this.array = arrayCopia;
             indiceAposFim -= 1;
+
         }
     }
 
     @Override
     public void removeFim() { // Completo, testado.
-        if (indiceAposFim != 1) {
+        if (indiceAposFim != 1) { // Verifica se ao menos um elemento já foi adicionado.
+            int[] arrayCopia = new int[this.array.length-1];
+            for (int i = 1; i < indiceAposFim-1; i++) {
+                arrayCopia[i] = this.array[i];
+            }
+            this.array = arrayCopia;
             indiceAposFim -= 1;
-            array[indiceAposFim] = 0;
         }
     }
 }
